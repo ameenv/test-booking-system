f78fd9120032a5280df6c7c35c95471cfad0cc8e
ameenv <ameenvg@gmail.com>
2025-10-29 20:15:59 +0530

    Clean single commit

--- src/components/EditBooking.jsx
+++ src/components/EditBooking.jsx
@@ -4,7 +4,7 @@ import Modal from 'react-bootstrap/Modal';
 import { Col, FloatingLabel, Row } from 'react-bootstrap';
 import { Form } from 'react-router-dom';
 
-function EditBooking({booking}) {
+function EditBooking({ booking }) {
     const [show, setShow] = useState(false);
 
     const handleClose = () => setShow(false);
@@ -12,7 +12,7 @@ function EditBooking({booking}) {
 
     const [bookService, setBookService] = useState(booking)
     console.log(bookService);
-    
+
     const handleEditBooking = async (booking) => {
 
         try {
@@ -38,19 +38,19 @@ function EditBooking({booking}) {
                 <Modal.Body>
                     <Row>
                         <Col>
-                        <input className='form-control w-50' placeholder='name' value={bookService.name} onChange={()=>setBookService(...bookService, e.target.value)}></input>
-                          
+                            <input className='form-control w-50' placeholder='name' value={bookService.name} onChange={() => setBookService(...bookService, e.target.value)}></input>
+
                         </Col>
-                       
+
                     </Row>
                 </Modal.Body>
                 <Modal.Footer>
                     <Button variant="secondary" onClick={handleClose}>
                         Close
                     </Button>
-                    {/* <Button variant="primary" onClick={() => handleEditBooking(booking)}>
+                    <Button variant="primary" onClick={() => handleEditBooking(booking)}>
                         update
-                    </Button> */}
+                    </Button>
                 </Modal.Footer>
             </Modal>
         </>
